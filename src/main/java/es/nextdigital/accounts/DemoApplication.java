de26f252b9ff1739022d0a76c83bb6b6ea2276bd
Javier Ramos Panduro <djavierramos@hotmail.com>
2023-12-27 17:12:18 +0100

    Crear controllers. No funcionales

--- src/main/java/es/nextdigital/accounts/DemoApplication.java
+++ src/main/java/es/nextdigital/accounts/DemoApplication.java
@@ -1,4 +1,4 @@
-package es.nextdigital.demo;
+package es.nextdigital.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/es/nextdigital/accounts/DemoApplicationTests.java
+++ src/test/java/es/nextdigital/accounts/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package es.nextdigital.demo;
+package es.nextdigital.accounts;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
